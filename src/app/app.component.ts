95400db65a402656534b30fccdb0de43db4f1984
Florian Chis <florian.chis@gmail.com>
2017-09-23 22:28:39 +0300

    working translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {TabsPage} from '../pages/tabs/tabs.page';
+import {BackgroundMode} from '@ionic-native/background-mode';
+import {TranslateService} from '@ngx-translate/core';
 
 
 @Component({
@@ -12,12 +14,39 @@ export class MyApp {
 
   rootPage = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  i18n: any[];
+
+  constructor(protected platform: Platform,
+              protected statusBar: StatusBar,
+              protected splashScreen: SplashScreen,
+              i18nService: TranslateService,
+              protected backgroundMode: BackgroundMode) {
+    i18nService.setDefaultLang('en');
+    i18nService.use(i18nService.getBrowserLang());
+    i18nService.get([
+      'nav.main',
+      'nav.info',
+      'nav.config',
+      'backgroundMode.defaultTitle',
+      'backgroundMode.defaultText'
+    ]).subscribe((value) => {
+      this.i18n = value;
+      this.initializeApp();
+    });
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.backgroundMode.enable();
+      this.backgroundMode.setDefaults({
+        title: this.i18n['backgroundMode.defaultTitle'],
+        text: this.i18n['backgroundMode.defaultText'],
+        ticker: ''
+      });
     });
   }
 }
