69abea38e1241331fc825aed5512ea2fe8fec519
Florian Chis <florian.chis@gmail.com>
2018-01-08 09:18:34 +0200

    refactoring

--- src/providers/chromecast.service.ts
+++ src/providers/chromecast.service.ts
@@ -18,8 +18,6 @@ export class ChromecastService {
   private i18n: any[];
   private cast: any;
   private session: any;
-  // prevent some initialize bug when opening the application for the first time
-  // private _reinitilize = true;
 
   private _state: ChromecastState = ChromecastState.DISABLED;
   public stateChanged: EventEmitter<ChromecastState> = new EventEmitter();
@@ -68,11 +66,6 @@ export class ChromecastService {
             // close session if existed
             this.close(ChromecastState.INITIALIZED);
             this.log.warn(this.i18n['providers.chromecast.receiverNotFound']);
-            // workaround
-            // if (this._reinitilize) {
-            //   this._reinitilize = false;
-            //   this.init();
-            // }
             break;
           }
           case this.cast.ReceiverAvailability.AVAILABLE: {
